4670b408500baeb283b2c15c9beb5a6733155303
Hugo Caille <hugo.caille@epitech.eu>
2017-08-16 17:09:41 +0200

    Starting again with clean basics

--- fnDate.php
+++ fnDate.php
@@ -1,29 +1,10 @@
 <?php
 
-<<<<<<< HEAD
-   /* class CustomDate
-    {
-        function getDate($Year)
-        {
-
-            $calendar = array();
-            $startDate = new DateTime($Year.'-01-01');
-
-            while ($startDate->format('Y') == $Year)
-            {
-                $Y = $startDate->format('Y');
-                $M = $startDate->format('m');
-                $d = $startDate->format('d');
-                $w = str_replace('0','7',$startDate->format('w'));
-
-                $calendar[$Y][$M][$d] = $w;
-=======
 class customDate
 {
   public $Year;
   public $Month;
   public $Day;
->>>>>>> origin/master
 
   public function __construct($Y, $M, $D)
   {
@@ -33,43 +14,6 @@ class customDate
   }
 }
 
-<<<<<<< HEAD
-            return $calendar;
-
-        }
-    }*/
-
-
-class customDate
-{
-   public $Year = 0;
-   public $Month = 0;
-   public $Day =0;
-
-   public function __construct($Y, $M, $D)
-   {
-    $Year = $Y;
-    $Month = $M;
-    $Day = $D;
-   }
-}
-
-
-class findDate
-{
-    function getDate($Year)
-    {
-        $calendar = array();
-        $startDate = new DateTime($Year.'-01-01');
-        while ($startDate->format('Y') == $Year)
-        {
-            $calendar = new customDate($startDate->format('Y'), $startDate->format('m'), $startDate->format('d'));
-            $startDate->add(new DateInterval('P1D'));
-        }
-        print_r($calendar);
-        return $calendar;
-    }
-=======
 class findDate
 {
   function getDate($Year)
@@ -84,5 +28,4 @@ class findDate
     print_r($calendar);
     return $calendar;
   }
->>>>>>> origin/master
 }
--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
 include ('fnDate.php');
-<<<<<<< HEAD
-
-
-    $Year = date('Y');
-
-    $findDate = new findDate();
-    $findDate->getDate($Year);
-
-
-
-
-
-
-=======
 findDate::getDate(2017);
->>>>>>> origin/master
